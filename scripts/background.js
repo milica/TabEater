399e6406042f5d3ec699b10da4b61a976321d7ce
borisarzentar <boris.arzentar@prelovac.com>
2014-02-06 17:30:12 +0100

    background modified

--- scripts/background.js
+++ scripts/background.js
@@ -5,25 +5,57 @@ if ('undefined' == typeof(tab)) {
 }
 
 if ('undefined' == typeof(tab.eater)) {
-    tab.eater = function() {
+    tab.eater = function () {
         var $private = {};
         $private.hidden = false;
+        $private.ACTION_IN_PROGRESS = false;
+        $private.currentWindow = null;
+
+        $private.hideTabs = function (tabs) {
+            var tab;
+            console.log(tabs);
+            chrome.storage.sync.get('TE.urls', function (obj) {
+                var urls = obj["TE.urls"], url, tabsToRemove = [];
+                for (url in urls) {
+                    for (tab in tabs) {
+                        if (tab.url.indexOf(url) === -1) {
+                            continue;
+                        } else {
+                            tabsToRemove.push(tab.id);
+                        }
+                    }
+                }
+                chrome.tabs.remove(tabsToRemove);
+            });
+        };
 
         $private.toggle = function () {
 
             if (this.hidden == false) {
+                console.log("BOSS, HIDE!");
+                chrome.tabs.query({
+                    currentWindow: true
+                }, $private.hideTabs);
 
             } else {
-
+                console.log("BOSS GONE, SHOW!");
             }
 
             this.hidden = !this.hidden;
         };
 
         var $shared = {
-            onClicked: function(info, tab) {
-
-                $private.toggle();
+            onClicked: function (info, tab) {
+                if (!$private.ACTION_IN_PROGRESS) {
+                    $private.ACTION_IN_PROGRESS = true;
+                    $private.toggle();
+                    $private.ACTION_IN_PROGRESS = false;
+                } else {
+                    return;
+                }
+            },
+            registerCurrentWindow: function (window) {
+                $private.currentWindow = window;
             }
         };
 
@@ -32,3 +64,4 @@ if ('undefined' == typeof(tab.eater)) {
 }
 
 chrome.browserAction.onClicked.addListener(tab.eater.onClicked);
+//chrome.windows.getCurrent(tab.eater.registerCurrentWindow);
