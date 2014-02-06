d32552a510d4f3cd730ca81a9ef964c7cd7f7039
borisarzentar <boris.arzentar@prelovac.com>
2014-02-06 16:37:59 +0100

    background modified

--- scripts/background.js
+++ scripts/background.js
@@ -1,16 +1,29 @@
 'use strict';
 
-if ('undefined' == typeof(tab))         {var tab = {};}
+if ('undefined' == typeof(tab)) {
+    var tab = {};
+}
 
-if ('undefined' == typeof(tab.eater))  {
+if ('undefined' == typeof(tab.eater)) {
     tab.eater = function() {
-        var $private = {
+        var $private = {};
+        $private.hidden = false;
 
+        $private.toggle = function () {
+
+            if (this.hidden == false) {
+
+            } else {
+
+            }
+
+            this.hidden = !this.hidden;
         };
 
         var $shared = {
-            onClicked: function(details) {
-                console.log("RADI!");
+            onClicked: function(info, tab) {
+
+                $private.toggle();
             }
         };
 
