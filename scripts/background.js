233f6f2475614d22568ec7ef8a0e220fcab6f9e5
borisarzentar <boris.arzentar@prelovac.com>
2014-02-07 10:55:50 +0100

    remove and add of tabs added

--- scripts/background.js
+++ scripts/background.js
@@ -1,48 +1,73 @@
 'use strict';
 
-if ('undefined' == typeof(tab)) {
-    var tab = {};
+if ('undefined' == typeof(TabEater)) {
+    var TabEater = {};
 }
 
-if ('undefined' == typeof(tab.eater)) {
-    tab.eater = function () {
-        var $private = {};
-        $private.hidden = false;
-        $private.ACTION_IN_PROGRESS = false;
-        $private.currentWindow = null;
+if ('undefined' == typeof(TabEater.background)) {
+    TabEater.background = function () {
+        var $private = {
+            hidden: false,
+            ACTION_IN_PROGRESS: false,
+            closedTabs: null
+        };
 
         $private.hideTabs = function (tabs) {
-            var tab;
             console.log(tabs);
             chrome.storage.sync.get('TE.urls', function (obj) {
-                var urls = obj["TE.urls"], url, tabsToRemove = [];
-                for (url in urls) {
-                    for (tab in tabs) {
-                        if (tabs[tab].url.indexOf(urls[url]) === -1) {
-                            continue;
-                        } else {
-                            tabsToRemove.push(tabs[tab].id);
-                        }
+                var urls = obj["TE.urls"], url, tabsToRemove = [], tab;
+                $private.closedTabs = [];
+//
+                for (tab in tabs) {
+                    var tabUrl = tabs[tab].url.split("/")[2];
+
+                    if (urls.indexOf(tabUrl) === -1) {
+                        continue;
+                    } else {
+                        tabsToRemove.push(tabs[tab].id);
+                        $private.closedTabs.push(tabs[tab]);
                     }
+
                 }
                 chrome.tabs.remove(tabsToRemove);
-            });
+                tabsToRemove = null;
+                urls = null;
+            })
         };
+
+        $private.showTabs = function () {
+            var tabs = $private.closedTabs, tab, index;
+
+            for (index in tabs) {
+                tab = {
+                    index: tabs[index].index,
+                    url: tabs[index].url
+                };
+                chrome.tabs.create(tab);
+            }
+            $private.closedTabs = null;
+        };
+
 
         $private.toggle = function () {
 
             if (this.hidden == false) {
+
                 console.log("BOSS, HIDE!");
                 chrome.tabs.query({
                     currentWindow: true
                 }, $private.hideTabs);
 
             } else {
+
                 console.log("BOSS GONE, SHOW!");
+                $private.showTabs();
+
             }
 
             this.hidden = !this.hidden;
         };
+
 
         var $shared = {
             onClicked: function (info, tab) {
@@ -58,6 +83,7 @@ if ('undefined' == typeof(tab.eater)) {
 
         return $shared;
     }();
+
 }
 
-chrome.browserAction.onClicked.addListener(tab.eater.onClicked);
+chrome.browserAction.onClicked.addListener(TabEater.background.onClicked);
